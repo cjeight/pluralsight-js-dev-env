249325b789f343e26006332e656eb5ba20bd044e
marc <cjeight@gmail.com>
2018-11-12 22:33:07 -0600

    From home.

--- src/index.test.js
+++ src/index.test.js
@@ -9,12 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html',() => {
-  it('should say hello', () => {
+  it('should have h1 that says Users', () => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function (err, window) {
        const h1 = window.document.getElementsByTagName('h1')[0];
-       expect(h1.innerHTML).to.equal("Hello World!");
-       done();
+       expect(h1.innerHTML).to.equal("Users");
+       // done();
        window.close();
     });
   })
